fa4200b88067d4bf9fc6e625e0823df79e361100
guyunying <1061449754@qq.com>
2019-09-09 16:41:50 +0800

    Lx03

---
+++ Service/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.UserInfo.Instance.GetCount());
+        }
+
+        // GET api/<controller>/5
+        [HttpPut("{username}")]
+        public ActionResult getUser(string username)
+        {
+            var result = DAL.UserInfo.Instance.GetModel(username);
+            if (result != null)
+                return Json(Result.Ok(result));
+            else
+                return Json(Result.Err("用户名不存在"));
+        }
+
+        // POST api/<controller>
+        [HttpPost("page")]
+        public ActionResult getPage([FromBody]Model.Page page)
+        {
+
+            var result = DAL.UserInfo.Instance.GetPage(page);
+            if (result.Count() == 0)
+                return Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+
+
+
+
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        public ActionResult Put([FromBody]Model.UserInfo users)
+        {
+            try
+            {
+                int n = DAL.UserInfo.Instance.Update(users);
+                if (n != 0)
+                    return Json(Result.Ok("修改成功"));
+                else
+                    return Json(Result.Err("用户名不存在"));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("密码、身份不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{username}")]
+        public ActionResult Delete(string username)
+        {
+            try
+            {
+                int n = DAL.UserInfo.Instance.Delete(username);
+                if (n != 0)
+                    return Json(Result.Ok("删除成功"));
+                else
+                    return Json(Result.Err("用户名不存在"));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("foreign"))
+                    return Json(Result.Err("发布了作品或活动的用户不能删除"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+    }
+}
+
+
